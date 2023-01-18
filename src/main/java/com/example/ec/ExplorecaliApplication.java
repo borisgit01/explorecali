c283406f19c9b3f555191897443840a0903f9181
borisgit01 <borisgodunovph@gmail.com>
2023-01-18 11:05:44 -0500

    Adding json file

--- src/main/java/com/example/ec/ExplorecaliApplication.java
+++ src/main/java/com/example/ec/ExplorecaliApplication.java
@@ -1,10 +1,18 @@
 package com.example.ec;
 
+import com.example.ec.service.TourPackageService;
+import com.example.ec.service.TourService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExplorecaliApplication {
+	@Autowired
+	private TourPackageService tourPackageService;
+
+	@Autowired
+	private TourService tourService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExplorecaliApplication.class, args);
