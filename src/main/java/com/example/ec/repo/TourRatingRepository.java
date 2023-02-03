dd0973f047e97b29d3634a13bda11d8ab7f0ad82
borisgit01 <borisgodunovph@gmail.com>
2023-02-03 10:07:40 -0500

    Done with chapter 3.1

--- src/main/java/com/example/ec/repo/TourRatingRepository.java
+++ src/main/java/com/example/ec/repo/TourRatingRepository.java
@@ -1,20 +1,44 @@
 package com.example.ec.repo;
 
 import com.example.ec.domain.TourRating;
-import com.example.ec.domain.TourRatingPk;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Tour Rating Repository Interface
+ *
+ * Created by Mary Ellen Bowman
+ */
 @RepositoryRestResource(exported = false)
-public interface TourRatingRepository extends CrudRepository<TourRating, TourRatingPk> {
-    List<TourRating> findByPkTourId(Integer tourId);
+public interface TourRatingRepository extends JpaRepository<TourRating, Integer> {
 
-    Optional<TourRating> findByPkTourIdAndPkCustomerId(Integer tourId, Integer customerId);
+    /**
+     * Lookup all the TourRatings for a tour.
+     *
+     * @param tourId is the tour Identifier
+     * @return a List of any found TourRatings
+     */
+    List<TourRating> findByTourId(Integer tourId);
 
-    Page<TourRating> findByPkTourId(Integer tourId, Pageable pageable);
+    /**
+     * Lookup a page of TourRatings for a tour.
+     *
+     * @param tourId tourId is the tour Identifier
+     * @param pageable details for the desired page
+     * @return a Page of any found TourRatings
+     */
+    Page<TourRating> findByTourId(Integer tourId, Pageable pageable);
+
+    /**
+     * Lookup a TourRating by the TourId and Customer Id
+     * @param tourId
+     * @param customerId
+     * @return TourRating if found, null otherwise.
+     */
+    Optional<TourRating> findByTourIdAndCustomerId(Integer tourId, Integer customerId);
 }
--- src/main/java/com/example/ec/web/RatingDto.java
+++ src/main/java/com/example/ec/web/RatingDto.java
@@ -1,26 +1,36 @@
 package com.example.ec.web;
 
-import com.example.ec.domain.TourRating;
-
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class RatingDto {
+/**
+ * Data Transfer Object for Rating a Tour.
+ *
+ * Created by Mary Ellen Bowman
+ */
+
+public class RatingDto  {
+
     @Min(0)
     @Max(5)
     private Integer score;
+
     @Size(max = 255)
     private String comment;
+
     @NotNull
     private Integer customerId;
 
-    public RatingDto(TourRating tourRating) {
-        this(tourRating.getScore(), tourRating.getComment(), tourRating.getPk().getCustomerId());
-    }
-
-    private RatingDto(Integer score, String comment, Integer customerId) {
+    /**
+     * Constructor to fully initialize the RatingDto
+     *
+     * @param score score
+     * @param comment comment
+     * @param customerId customer identifier
+     */
+    public RatingDto(Integer score, String comment, Integer customerId) {
         this.score = score;
         this.comment = comment;
         this.customerId = customerId;
@@ -51,4 +61,5 @@ public class RatingDto {
     public void setCustomerId(Integer customerId) {
         this.customerId = customerId;
     }
+
 }
